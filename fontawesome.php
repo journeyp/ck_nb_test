794e455ed07b47e21e2ebb75d1e176a7bda7ae55
journeyp <la_junny@naver.com>
2019-07-01 01:35:15 +0900

    Add_newFile_helloworld

---
+++ fontawesome.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+	<head>
+		<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0, user-scalable=no">
+		<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
+		<style>
+			*{
+				text-decoration: none;
+			}
+			#social{
+				color: #9f9f9f;
+				font-size: 20px;
+				border: 2px solid #9f9f9f;
+				border-radius: 50%;
+				padding: 9px 10px;
+				margin: 20px 2px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="wrap">
+			<div class="sns">
+				<a href="#">
+					<i class="fa fa-twitter" id="social"></i>
+				</a>
+				<a href="#">
+					<i class="fa fa-facebook-f" id="social" style="padding: 9px 13px;"></i>
+				</a>
+				<a href="#">
+					<i class="fa fa-instagram" id="social"></i>
+				</a>
+			</div>
+		</div>
+	</body>
+</html>
